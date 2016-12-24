fc7206f0218d1be018b890b9f54d49100d0d1e1b
岳克奎 <738816656@qq.com>
2016-12-24 20:25:46 +0800

    Initial Commit

---
+++ SlienceLiveTV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlienceLiveTV
+//
+//  Created by 岳克奎 on 16/12/24.
+//  Copyright © 2016年 岳克奎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlienceLiveTV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlienceLiveTV
+//
+//  Created by 岳克奎 on 16/12/24.
+//  Copyright © 2016年 岳克奎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
