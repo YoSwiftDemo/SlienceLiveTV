fb0b9a1ebd5c237f8f968ec1592fff1a7a20ec10
岳克奎 <738816656@qq.com>
2016-12-26 09:16:28 +0800

    配置cocopods&&AFN
    配置cocopods&&AFN

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+#use_frameworks!
+target 'SlienceLiveTV' do
+  pod 'AFNetworking'
+
+end
---
+++ Pods/Target Support Files/Pods-SlienceLiveTV/Pods-SlienceLiveTV-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SlienceLiveTV : NSObject
+@end
+@implementation PodsDummy_Pods_SlienceLiveTV
+@end
